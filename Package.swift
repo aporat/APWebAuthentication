91fa5cee8219deebc801bd777fcff556d620eac9
aporat28 <aporat28@gmail.com>
2026-09-15 08:15:57 -0400

    bump

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "APWebAuthentication",
     platforms: [
-        .iOS(.v18)
+        .iOS(.v17)
     ],
     products: [
         .library(
